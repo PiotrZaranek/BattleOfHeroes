412a4a1e839aed9e455eeedb47b2a3e86286b170
PiotrZaranek <piotrek.zaranek@gmail.com>
2022-11-25 11:29:18 +0100

    Add Interface and Class Move

---
+++ BattleOfHeros.Domain/Abstract/IMove.cs
@@ -0,0 +1,16 @@
+﻿using BattleOfHeroes.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleOfHeroes.Domain.Abstract
+{
+    internal interface IMove
+    {
+        Hero Hero { get; set; }
+        IAction Action { get; set; }
+        Hero Target { get; set; }
+    }
+}
---
+++ BattleOfHeros.Domain/Concrete/Move.cs
@@ -0,0 +1,24 @@
+﻿using BattleOfHeroes.Domain.Abstract;
+using BattleOfHeroes.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleOfHeroes.Domain.Concrete
+{
+    public class Move : IMove
+    {
+        public Hero Hero { get; set; }
+        public IAction Action { get; set; }
+        public Hero Target { get; set; }
+
+        public Move()
+        {
+            Hero = null;
+            Action = null;
+            Target = null;
+        }
+    }
+}
