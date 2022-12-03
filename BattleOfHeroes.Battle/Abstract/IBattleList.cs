0667f50e0adba5219ff95fd5bd03f62d0ff33155
Piotr Zaranek <piotrek.zaranek@gmail.com>
2022-12-03 14:41:10 +0100

    Add IBattleList interface.

---
+++ BattleOfHeroes.Battle/Abstract/IBattleList.cs
@@ -0,0 +1,21 @@
+﻿using BattleOfHeroes.Domain.Common;
+using BattleOfHeroes.Domain.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleOfHeroes.BattleApp.Abstract
+{
+    internal interface IBattleList
+    {
+        List<PlayersMoves> PlayersMoves { get; set; }
+
+        void AddMove(int turn, Player player, Operation operation);
+        void AddMove(int turn, Player player, Skill skill);
+        void AddMove(int turn, Player player, string message);
+
+        void ListMoves();
+    }
+}
