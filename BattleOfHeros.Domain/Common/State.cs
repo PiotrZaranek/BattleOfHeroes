7ae985573c4c9dd5c0f9c9df915689fee3e91664
Piotr Zaranek <piotrek.zaranek@gmail.com>
2022-12-03 15:29:57 +0100

    Added State class.

---
+++ BattleOfHeros.Domain/Common/State.cs
@@ -0,0 +1,25 @@
+﻿using BattleOfHeroes.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleOfHeroes.Domain.Common
+{
+    public abstract class State : IState
+    {
+        public string Name { get; set; }
+        public int Time { get; set; }
+        public char Type { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public void Execute()
+        {
+            if(Time > 0)
+            {
+                Time--;
+            }                        
+        }
+    }
+}
