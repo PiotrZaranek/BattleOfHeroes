6fe40d2995d4e05b2c9c152d69dadd6be0176726
Piotr Zaranek <piotrek.zaranek@gmail.com>
2022-12-03 14:40:39 +0100

    Add IBattle interface.

---
+++ BattleOfHeroes.Battle/Abstract/IBattle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using BattleOfHeroes.Domain.Concrete;
+using BattleOfHeroes.BattleApp.Concrete;
+
+namespace BattleOfHeroes.Domain.Abstract
+{
+    internal interface IBattle
+    {
+        List<Player> Players { get; }
+        int Turn { get; set; }
+        BattleList BattleList { get; set; }
+
+        void Main();      
+    }
+}
