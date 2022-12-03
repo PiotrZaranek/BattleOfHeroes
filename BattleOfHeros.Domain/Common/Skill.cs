97d3f0080bb8d868dd17ed92e53c5cdcc3cdd0fd
Piotr Zaranek <piotrek.zaranek@gmail.com>
2022-12-03 15:29:35 +0100

    Added parameter to Action() method. Added implemented to UpdateMana() method.

--- BattleOfHeros.Domain/Common/Skill.cs
+++ BattleOfHeros.Domain/Common/Skill.cs
@@ -7,13 +7,19 @@ using BattleOfHeroes.Domain.Abstract;
 
 namespace BattleOfHeroes.Domain.Common
 {
-    public abstract class Skill : ISkill, IAction
+    public abstract class Skill : ISkill
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int NeedMana { get; set; }
         public char Type { get; set; }
         public bool IsActive { get; set; }
-        public abstract void Action(Hero hero);        
+        public bool IsAura { get; set; }
+
+        public abstract void Action(Hero hero, Hero target);
+        public void UpdateMana(Hero hero)
+        {
+            hero.Mana -= NeedMana;
+        }
     }
 }
