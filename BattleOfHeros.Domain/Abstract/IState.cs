94182301d9ed10405f1e1fbb0171793d64f93f86
Piotr Zaranek <piotrek.zaranek@gmail.com>
2022-12-03 15:20:59 +0100

    Added new interface IState.

---
+++ BattleOfHeros.Domain/Abstract/IState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleOfHeroes.Domain.Abstract
+{
+    public interface IState
+    {
+        string Name { get; set; }
+        int Time { get; set; }
+        char Type { get; set; }
+        ConsoleColor Color { get; set; }
+        void Execute();        
+    }
+}
