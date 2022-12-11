b10a1e39f0357f6811fb684b66bb4087e661bba8
Piotr Zaranek <piotrek.zaranek@gmail.com>
2022-12-11 14:24:13 +0100

    Added IPlayerMove interface.

---
+++ BattleOfHeros.Domain/Abstract/IPlayerMove.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleOfHeroes.Domain.Abstract
+{
+    internal interface IPlayerMove
+    {
+        int Turn { get; }
+        string PlayerName { get; }
+        string HeroName { get; }
+        string Action { get; }
+
+        void ShowMove();
+    }
+}
