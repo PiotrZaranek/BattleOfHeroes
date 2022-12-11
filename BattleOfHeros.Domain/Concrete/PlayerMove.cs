787524e31f2b4621086295fc9d73261549b12307
Piotr Zaranek <piotrek.zaranek@gmail.com>
2022-12-11 14:24:34 +0100

    Added PlayerMove class.

---
+++ BattleOfHeros.Domain/Concrete/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BattleOfHeroes.Domain.Abstract;
+
+namespace BattleOfHeroes.Domain.Concrete
+{
+    public class PlayerMove : IPlayerMove
+    {
+        public int Turn { get; }
+        public string PlayerName { get; }
+        public string HeroName { get; }
+        public string Action { get; }
+
+        public PlayerMove(int turn, string playerName, string heroName, string action)
+        {
+            Turn = turn;
+            PlayerName = playerName;
+            HeroName = heroName;
+            Action = action;
+        }
+
+        public void ShowMove()
+        {
+            Console.Write($"|{Turn}-{PlayerName}-{HeroName}-{Action}|");
+        }
+    }
+}
