5ce5cb1a5ff9a95ee50f1cb6a6928a1e284f2372
Piotr Zaranek <piotrek.zaranek@gmail.com>
2022-12-11 14:18:15 +0100

    Added unit tests for Attack class.

--- BattleOfHeroes.UnitTests/DomainTests/ConcreteOperationTests/AttackTests.cs
+++ BattleOfHeroes.UnitTests/DomainTests/ConcreteOperationTests/AttackTests.cs
@@ -1,12 +1,34 @@
 ﻿using BattleOfHeroes.Domain.Common;
 using BattleOfHeroes.Domain.ConcreteHero;
 using BattleOfHeroes.Domain.ConcreteOperation;
+using FluentAssertions;
 using NuGet.Frameworks;
 
 namespace BattleOfHeroes.UnitTests.DomainTests.ConcreteOperationTests
 {
     public class AttackTests
     {
-        
+        [Fact]
+        public void AttackAction_HeroLife_Expect_LifeReducedByFrom5To10()
+        {
+            Hero hero = new Paladin(1);
+            Operation oper = new Attack(1);
+
+            oper.Action(hero, 15);  
+
+            hero.Life.Should().BeInRange(hero.MaxLife - 10, hero.MaxLife - 5);
+        }
+
+        [Fact]
+        public void AttackAction_HeroLifeOperationIsActiveIsFalse_Expect_NoHeroLifeChanges()
+        {
+            Hero hero = new Paladin(1);
+            Operation oper = new Attack(1);
+
+            oper.IsActive = false;
+            oper.Action(hero, 15);
+
+            hero.Life.Should().Be(hero.MaxLife);
+        }
     }
 }
