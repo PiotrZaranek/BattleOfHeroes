b1b6a1bd8353ea912349ad1d13a02533f35d657b
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-01-08 18:51:24 +0100

    Added BattleStory class.

---
+++ BattleOfHeroes.Battle/Concrete/BattleStory.cs
@@ -0,0 +1,25 @@
+﻿using BattleOfHeroes.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleOfHeroes.BattleApp.Concrete
+{
+    public class BattleStory : IBattleStory
+    {
+        public int Id { get; set; }
+        public string PlayerOne { get; set; }
+        public string PlayerTwo { get; set; }
+        public BattleList BattleList { get; set; }
+
+        public BattleStory(int id, string po, string pt, BattleList bl)
+        {
+            Id = id;
+            PlayerOne = po;
+            PlayerTwo = pt;
+            BattleList = bl;
+        }
+    }
+}
