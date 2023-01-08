a43a8e63acce5063c4e33010f2e4ecf8f89519b3
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-01-08 18:55:03 +0100

    Added new property BattleStories type List<BattleStory>. Added new private field path type string. Added constructor no parameters. Added new method ListMoves. Added new private method ReadBattleFromFile(). Added new method SaveBattleStoryToFile().

--- BattleOfHeros.App/Concrete/BattleServices.cs
+++ BattleOfHeros.App/Concrete/BattleServices.cs
@@ -6,12 +6,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using BattleOfHeroes.BattleApp.Concrete;
+using BattleOfHeroes.App.Helpers;
 
 namespace BattleOfHeroes.App.Concrete
 {
     public class BattleServices
     {
         public int GameMode { get; set; }
+        public List<BattleStory> BattleStories { get; set; }
+
+        private string path = @"C:\temp\Battle.json";
+
+        public BattleServices()
+        {
+            BattleStories = new List<BattleStory>();
+            ReadBattleFromFile();
+        }
 
         public void ChooseHeroPlayer(List<Hero> heroes, int id, PlayerServices playerServices)
         {
@@ -30,10 +42,32 @@ namespace BattleOfHeroes.App.Concrete
         public void StartBattle(Battle battle, MenagerBattleServices menagerBattleServices)
         {
             battle.Main();
-            Console.WriteLine("Siema!");
+            BattleStories.Add(new BattleStory(BattleStories.Count + 1, battle.Players[0].PlayerName, battle.Players[1].PlayerName, battle.BattleList));                        
         }
 
+        public void ListMoves(BattleList bl) 
+        {
+            Console.Clear();
+            bl.ListMoves();
+        }
         
-        
+        private void ReadBattleFromFile()
+        {
+            using StreamReader sr = new StreamReader(path);
+            string input = sr.ReadToEnd();
+
+            if(input.Length != 0)
+            {
+                BattleStories = JsonConvert.DeserializeObject<List<BattleStory>>(input);
+            }            
+        }
+
+        public void SaveBattleStoryToFile()
+        {            
+            using StreamWriter sw = new StreamWriter(path);
+            using JsonWriter jw = new JsonTextWriter(sw);
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Serialize(jw, BattleStories);
+        }
     }
 }
